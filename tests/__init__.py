8d9786deec7c0425a8a5b5de8ce7cc3b52b4f8d1
Samuel Kuhn <samuel.kuhn@yoga>
2025-05-05 14:42:59 +0200

    updated test_version_matches to be os unspecific

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,5 @@
 import importlib.metadata
-import sys
+import sys, os
 
 import ics
 
@@ -9,8 +9,10 @@ def test_version_matches():
     print(dist.version, dist.__dict__, sys.path, ics.__path__)
     assert len(ics.__path__) == 1
     ics_path = ics.__path__[0]
+
+    site_packages_folder = os.path.sep + "site-packages" + os.path.sep
     assert (
-        "/site-packages/" in ics_path and not "/src" in ics_path
+        site_packages_folder in ics_path and not "/src" in ics_path
     ), f"ics should be imported from package not from sources '{ics_path}' for testing"
     for path in sys.path:
         assert not path.endswith("/src"), (
